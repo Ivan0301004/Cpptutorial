25c89fd2dfedc3cecdadd304b4bb2f0197a1a9e6
Ivan0301004 <ignizbones@gmail.com>
2022-07-01 20:33:41 -0500

    new

--- lvalue/referencias.cpp
+++ lvalue/referencias.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 
-int main(){ 
+int main()
+{
    int c{34};
    std::cout << c << '\n';
    {
-      int& ref {c}; // declaracion de referencia lvalue
-      ref = 78; //  ref es alias de C por lo que C ahora vale 78
+      int &ref{c};              // declaracion de referencia lvalue
+      ref = 78;                 //  ref es alias de C por lo que C ahora vale 78
       std::cout << ref << '\n'; // imprimor la referencia
       ref = 89;
       std::cout << ref << '\n';
       c = 9;
       std::cout << c << '\n';
+      int &y{ref};
+      std::cout << y << '\n';
    }
    std::cout << c << '\n';
    c = 90;
    std::cout << c << '\n'; // como  cambiamos el valor de la variable C con una referencia
    // ahora variable C vale 78
-return 0;
+   return 0;
 }
