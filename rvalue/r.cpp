80ea99742b8df56c55d13ea81c1860f5ba2cd9a4
Ivan0301004 <ignizbones@gmail.com>
2022-07-06 22:47:15 -0500

    n

--- rvalue/r.cpp
+++ rvalue/r.cpp
@@ -6,7 +6,7 @@ int cinco(){
 
 int main(){
 
-   int x {5}; // numero 5 es Rvalue
+   int x{5}; // numero 5 es Rvalue
    const double e{6.7}; // numero 6.7 es rvalue
    std::cout << x << '\n'; // x es lvalue modificable 
    std::cout << e << '\n'; // e es lvalue no modificable ya que es const
@@ -14,9 +14,15 @@ int main(){
    std::cout << 3+6 << '\n'; // es una expresion rvalue
    std::cout << static_cast<int>(e) << '\n'; // variable "e" a un int rvalue
 
+   int rf{45}; // variable lvalue
+   std::cout << rf << '\n';
+   rf = 34; // variable rvalue
+   std::cout << rf << '\n';
 
 
 
+
+   
 
    return 0;
 }
